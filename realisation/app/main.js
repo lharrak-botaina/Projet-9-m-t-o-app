3641b280a16861c24d7fe5e11d359743fc5a5938
lharrak-botaina <lharrak.botaina@gmail.com>
2022-01-19 04:46:07 -0800

    add time

--- realisation/app/main.js
+++ realisation/app/main.js
@@ -26,9 +26,18 @@ async function onSearch() {
 function display(currentForecast) {
     document.getElementById('city').textContent= currentForecast.city
     document.getElementById('temp').textContent= currentForecast.temp+ ""+ "°C"
-    const d = new Date();
-document.getElementById("date").innerHTML = d.();  
+    
+
     document.getElementById('desc').textContent= currentForecast.description
     document.getElementById('country').textContent= currentForecast.humidity
 
 }
+
+    let dateToday =document.getElementById("date");
+  let today = new Date();
+  let day =`${today.getDate() < 10?"0" :""}${today.getDate()}`
+    let month =`${(today.getMonth()+1) < 10?"0" :""}${today.getMonth()+1}`
+
+    let year =today.getFullYear();
+
+    dateToday.textContent =`${day}/${month}/${year}`
